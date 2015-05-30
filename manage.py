9b11f6d94119b99cf8d43723cf57cde569fd090b
Brandon Williams <williamsbdev@gmail.com>
2015-05-29 21:49:55 -0500

    [BUILD] adding/modifying files to deploy to Heroku

--- manage.py
+++ manage.py
@@ -3,6 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.{}".format(sys.argv[1]))
+    settings_file = 'production'
+    args = sys.argv
+    if sys.argv[1] == 'test':
+        settings_file = 'test'
+        args = [sys.argv[0]] + sys.argv[2:]
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.{}".format(settings_file))
     from django.core.management import execute_from_command_line
-    execute_from_command_line([sys.argv[0]] + sys.argv[2:])
+    execute_from_command_line(args)
--- project/settings/test.py
+++ project/settings/test.py
@@ -3,4 +3,3 @@ from project.settings.base import *
 INSTALLED_APPS += ('django_nose', )
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
