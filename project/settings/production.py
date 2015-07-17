3f759e5bc6e9d5a97ddf6faab75094ebb63c1e66
Brandon Williams <williamsbdev@gmail.com>
2015-07-16 21:35:20 -0500

    [BUILD] fixing production settings (database and debug)

--- project/settings/production.py
+++ project/settings/production.py
@@ -1,2 +1,12 @@
 from project.settings.base import *
 import dj_database_url
+
+DEBUG = False
+
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
