2f5507029b712c0f63b2d14eb197d8d76683e329
Brandon Williams <williamsbdev@gmail.com>
2015-07-19 15:38:48 -0500

    [ENHANCEMENT] adding auth urls

--- project/urls.py
+++ project/urls.py
@@ -7,5 +7,6 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^$', 'project.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url('', include('django.contrib.auth.urls', namespace='auth')),
     url('', include('social.apps.django_app.urls', namespace='social')),
 ]
