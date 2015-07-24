d1ec4a1ef4ba34a38c6b7d61373aa99fb0137788
Brandon Williams <williamsbdev@gmail.com>
2015-07-23 22:06:19 -0500

    [FEATURE] adding the ability to be an OAuth2 provider

--- project/settings/base.py
+++ project/settings/base.py
@@ -13,6 +13,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'provider',
+    'provider.oauth2',
     'social.apps.django_app.default',
 )
 
